479ea2faf30ef1d8246273489004e3e21e2b4f9d
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-08-11 22:24:31 -0700

    Library is now module

--- lib/hoja-lib-rp2040/include/devices/nspi.h
+++
@@ -1,9 +0,0 @@
-#ifndef NSPI_H
-#define NSPI_H
-
-#include "hoja_includes.h"
-
-void nspi_load_buffer();
-void nspi_init();
-
-#endif
--- lib/hoja-lib-rp2040/include/input/safemode.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAFEMODE_H
-#define SAFEMODE_H
-
-#include "hoja_includes.h"
-
-void safe_mode_task(button_data_s *in);
-bool safe_mode_check();
-
-#endif
