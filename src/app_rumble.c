99558ca1fbebfae200db92f221914b783afacb18
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-10-02 17:18:23 -0700

    Rumble adjust for accuracy

--- src/app_rumble.c
+++ src/app_rumble.c
@@ -4,69 +4,73 @@
 
 const uint32_t _rumble_interval = 8000;
 int _rumble_intensity = 0;
+
 int _rumble_current = 0;
 
 #define RUMBLE_MAX 100
 static uint8_t _rumble_max = RUMBLE_MAX;
 
-static bool _rumble = false;
 static bool _declining = false;
+
+/**
+ * pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 0);
+ * pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, _rumble_current);
+*/
 
 void app_rumble_task(uint32_t timestamp)
 {
-    static int lvl = 0;
-
     if(interval_run(timestamp, _rumble_interval))
     {
-        if ( (lvl < _rumble_intensity) && !_declining)
+        if(_declining)
         {
-            lvl += 10;
-            if (lvl >= _rumble_intensity)
+            _rumble_current -= 20;
+            if (_rumble_current < 0)
             {
-                lvl = _rumble_intensity;
-                _declining = true;
+                _rumble_current = 0;
             }
-            pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 0);
-            pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, lvl);
+            _rumble_intensity = _rumble_current;
         }
-        else if (lvl > _rumble_current)
+        else
         {
-            lvl -= 20;
-            if (lvl <= 0)
+            if (_rumble_current < _rumble_intensity)
             {
-                _declining = false;
-                lvl = 0;
-                pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, 0);
-                pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 255);
+                _rumble_current += 10;
+
+                if (_rumble_current >= _rumble_intensity)
+                {
+                    _rumble_current = _rumble_intensity;
+                }
             }
-            else
-            {
-                pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, lvl);
-            }
-            _rumble_intensity = lvl;
+            else _declining = true;
+
         }
+        
+        pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, (!_rumble_current) ? 255 : 0);
+        pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, (_rumble_current > 0) ? _rumble_current : 0);
+
     }
 }
 
 void cb_hoja_rumble_enable(float intensity)
 {
     if(intensity > 1.0f) intensity = 1.0f;
-    
-    float p = _rumble_max * intensity;
-    uint16_t tmp = (uint16_t) p;
-    if(tmp>_rumble_intensity)
+
+    if(!intensity)
     {
-        _rumble_intensity = tmp;
+        // Do nothing
+    }
+    else 
+    {
+        float p = _rumble_max * intensity;
+        uint16_t tmp = (uint16_t) p;
+        if(tmp>_rumble_intensity)
+        {
+            _rumble_intensity = tmp;
+        }
         _declining = false;
     }
-
-    if(!intensity) 
-    {
-        _rumble_current = 0;
-
-    }
-    else _rumble_current = tmp;
-
+    
+    
 }
 
 void cb_hoja_set_rumble_intensity(uint8_t intensity)
