ac7996804831bcb6fd1a15c5d98fe9fef2ee2a8b
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-10-02 17:43:12 -0700

    Update build (again)

--- src/app_rumble.c
+++ src/app_rumble.c
@@ -3,7 +3,11 @@
 #include "main.h"
 
 const uint32_t _rumble_interval = 8000;
-int _rumble_intensity = 0;
+
+
+int _rumble_cap = 0;
+
+int _rumble_floor = 0;
 
 int _rumble_current = 0;
 
@@ -21,28 +25,20 @@ void app_rumble_task(uint32_t timestamp)
 {
     if(interval_run(timestamp, _rumble_interval))
     {
-        if(_declining)
+        if (_rumble_current < _rumble_cap)
         {
-            _rumble_current -= 20;
-            if (_rumble_current < 0)
-            {
-                _rumble_current = 0;
-            }
-            _rumble_intensity = _rumble_current;
+            _rumble_current += 20;
         }
         else
         {
-            if (_rumble_current < _rumble_intensity)
+            _rumble_current -= 10;
+            
+            if (_rumble_current <= _rumble_floor)
             {
-                _rumble_current += 10;
-
-                if (_rumble_current >= _rumble_intensity)
-                {
-                    _rumble_current = _rumble_intensity;
-                }
+                _rumble_current = _rumble_floor; 
             }
-            else _declining = true;
 
+            _rumble_cap = _rumble_current;
         }
         
         pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, (!_rumble_current) ? 255 : 0);
@@ -55,22 +51,15 @@ void cb_hoja_rumble_enable(float intensity)
 {
     if(intensity > 1.0f) intensity = 1.0f;
 
-    if(!intensity)
+    float p = _rumble_max * intensity;
+    uint16_t tmp = (uint16_t) p;
+
+    _rumble_floor = tmp;
+    if(tmp>_rumble_cap)
     {
-        // Do nothing
+        _rumble_cap = tmp;
     }
-    else 
-    {
-        float p = _rumble_max * intensity;
-        uint16_t tmp = (uint16_t) p;
-        if(tmp>_rumble_intensity)
-        {
-            _rumble_intensity = tmp;
-        }
-        _declining = false;
-    }
-    
-    
+
 }
 
 void cb_hoja_set_rumble_intensity(uint8_t intensity)
