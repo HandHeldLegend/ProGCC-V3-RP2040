70eb36100d1a6e0bd03bd0f1bb12c9afbee9aefe
Mitch Cairns <mitch.cairns@handheldlegend.com>
2024-06-04 20:51:05 -0700

    Trigger RGB Support

--- include/board_config.h
+++ include/board_config.h
@@ -4,7 +4,7 @@
 // Device stuff
 #define HOJA_DEVICE_ID  0xA001 // DO NOT CHANGE FOR PROGCC 3
 
-#define HOJA_FW_VERSION 0x0A42
+#define HOJA_FW_VERSION 0x0A43
 
 // RGB Stuff
 #define HOJA_RGB_PIN 15
